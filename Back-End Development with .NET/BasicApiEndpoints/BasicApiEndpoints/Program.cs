8c918fefbade82569e5a21685bb738c3c922706d
Werner Kruger <werner.kruger.work@gmail.com>
2026-01-22 11:02:55 +0200

    File Structure And Serialization projects for full repo update

--- Back-End Development with .NET/BasicApiEndpoints/BasicApiEndpoints/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/downloads", () => "Downloads Page");
-app.MapPut("/", () => "PUT Request Received");
-
-app.Run();
